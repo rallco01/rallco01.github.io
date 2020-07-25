91af41e8122876752db0b4ad206570acdf5b86f0
rallco01 <62082846+rallco01@users.noreply.github.com>
2020-07-26 03:07:33 +1000

    Update to include hidden page with experiemental systems

---
+++ Scripts/LoadingCocktailJSON.js
@@ -0,0 +1,69 @@
+function testingJSFile()
+{
+	alert("Test");
+}
+
+function readCocktailJSONFile()
+{
+	//referrence the display area where all cards will be displayed
+	displayAreaDiv = document.getElementById("CocktailDisplayArea");
+
+	//read the JSON File and send it to the console for debugging
+	console.log(DATA);
+	obj = DATA;
+
+	for (i = 0; i < obj.Cocktails.length; i++) {
+		//-------------=======Variable Creation=======-------------//
+		//Create variables for all the informtaion stored in each cocktail object
+		cocktailName = obj.Cocktails[i].Name;
+		cocktailIngredientNames = [];
+		cocktailIngredientAmounts = [];
+		//store all ingredient names and amounts in arrays to be accessed via indicies later
+		for (j = 0; j < obj.Cocktails[i].Ingredients.length; j++) {
+			cocktailIngredientNames.push(obj.Cocktails[i].Ingredients[j].Name);
+			cocktailIngredientAmounts.push(obj.Cocktails[i].Ingredients[j].Amount);
+		}
+		//continue storing information to be referenced
+		cocktailMethod = obj.Cocktails[i].Method;
+		cocktailImage = obj.Cocktails[i].Image;
+
+		//create new element IDs based on cocktail names, as each cocktail should have a unique name
+		cardDivID = cocktailName + ' Card'
+		imageDivID = cocktailName + ' Image';
+		textBoxDivID = cocktailName + ' Text Box';
+		ingredientsListID = cocktailName + ' List'
+		//create the card div and a variable to store it for referrencing
+		displayAreaDiv.innerHTML += '<div class="Card" id="' + cardDivID + '"></div>';
+		CardDiv = document.getElementById(cardDivID);
+
+		//create other divs for image and text
+		CardDiv.innerHTML += '<div class="ImageHolder" id="' + imageDivID + '"></div>';
+		CardDiv.innerHTML += '<div class="TextBox" id="' + textBoxDivID + '"></div>';
+
+		//make reference to those divs for use later
+		imageDiv = document.getElementById(imageDivID);
+		textBoxDiv = document.getElementById(textBoxDivID);
+
+		//-------------=======Information Displaying=======-------------//
+		//place the image in the image div
+		imageDiv.innerHTML += '<img src="' + cocktailImage + '" />'
+
+		//place information in textbox div
+		textBoxDiv.innerHTML += '<h1>' + cocktailName + '</h1>';
+		textBoxDiv.innerHTML += '<h2>Ingredients</h2>';
+
+		//create list of ingredients
+		textBoxDiv.innerHTML += '<ul id="' + ingredientsListID + '"></ul>';
+		//make reference to that list
+		ingredientList = document.getElementById(ingredientsListID);
+		//put ingredients in the list
+		for (j = 0; j < obj.Cocktails[i].Ingredients.length; j++) {
+			ingredientList.innerHTML += '<li>' + cocktailIngredientNames[j] + ', ' + cocktailIngredientAmounts[j] + '</ul>';
+		}
+
+		textBoxDiv.innerHTML += '<h2>Method</h2>';
+		textBoxDiv.innerHTML += '<p>' + cocktailMethod + '</p>';
+	}
+}
+
+
---
+++ Scripts/TestJSONFile.js
@@ -0,0 +1,43 @@
+var DATA = {
+  "Cocktails" : [
+    {
+      "Name": "Blue Island Iced Tea",
+      "Ingredients": [
+        {
+          "Name": "Vodka",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Gin",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "White Rum",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Tequila",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Blue Curacao",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Lemon Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Lime Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Coke",
+          "Amount": "To Top"
+        }
+      ],
+      "Method": "In a shaker, combine all the spirits, lemon and lime juice and shake until properly chilled, pour into a highball glass and top with coke. Optionally, garnish with lemon wheel.",
+      "Image": "CocktailPhotos\\BlueIslandIcedTea.jpg"
+    }
+  ]
+}
