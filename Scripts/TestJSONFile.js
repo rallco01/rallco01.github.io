6eef0e36febb78f55075b85955be81eab88281be
rallco01 <62082846+rallco01@users.noreply.github.com>
2020-10-20 15:07:01 +1100

    added new layout system testing page

--- Scripts/TestJSONFile.js
+++ Scripts/TestJSONFile.js
@@ -40,6 +40,293 @@ var DATA = {
       "Method": "In a shaker, combine all the spirits, lemon and lime juice and shake until properly chilled, pour into a highball glass and top with coke. Optionally, garnish with lemon wheel.",
       "Image": "CocktailPhotos\\BlueIslandIcedTea.jpg",
       "ImageCredits": "This horrible photo was taken by Ronin on a crappy smart phone, more professional photos to come"
+    },
+    {
+      "Name": "Blue Island Iced Tea 1",
+      "Description": "The Blue Island Iced Tea is a simple modification of the Long Island Iced tea, simply replacing the triple sec with blue curacao. Due to the similar taste profiles, the taste of the drink isn't affected, however the result is a drink which naturally creates beautifully contrasted layers, while still remaining a tasty classic",
+      "Ingredients": [
+        {
+          "Name": "Vodka",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Gin",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "White Rum",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Tequila",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Blue Curacao",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Lemon Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Lime Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Coke",
+          "Amount": "To Top"
+        }
+      ],
+      "Method": "In a shaker, combine all the spirits, lemon and lime juice and shake until properly chilled, pour into a highball glass and top with coke. Optionally, garnish with lemon wheel.",
+      "Image": "CocktailPhotos\\BlueIslandIcedTea.jpg",
+      "ImageCredits": "This horrible photo was taken by Ronin on a crappy smart phone, more professional photos to come"
+    },
+    {
+      "Name": "Blue Island Iced Tea 2",
+      "Description": "The Blue Island Iced Tea is a simple modification of the Long Island Iced tea, simply replacing the triple sec with blue curacao. Due to the similar taste profiles, the taste of the drink isn't affected, however the result is a drink which naturally creates beautifully contrasted layers, while still remaining a tasty classic",
+      "Ingredients": [
+        {
+          "Name": "Vodka",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Gin",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "White Rum",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Tequila",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Blue Curacao",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Lemon Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Lime Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Coke",
+          "Amount": "To Top"
+        }
+      ],
+      "Method": "In a shaker, combine all the spirits, lemon and lime juice and shake until properly chilled, pour into a highball glass and top with coke. Optionally, garnish with lemon wheel.",
+      "Image": "CocktailPhotos\\BlueIslandIcedTea.jpg",
+      "ImageCredits": "This horrible photo was taken by Ronin on a crappy smart phone, more professional photos to come"
+    },
+    {
+      "Name": "Blue Island Iced Tea 3",
+      "Description": "The Blue Island Iced Tea is a simple modification of the Long Island Iced tea, simply replacing the triple sec with blue curacao. Due to the similar taste profiles, the taste of the drink isn't affected, however the result is a drink which naturally creates beautifully contrasted layers, while still remaining a tasty classic",
+      "Ingredients": [
+        {
+          "Name": "Vodka",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Gin",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "White Rum",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Tequila",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Blue Curacao",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Lemon Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Lime Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Coke",
+          "Amount": "To Top"
+        }
+      ],
+      "Method": "In a shaker, combine all the spirits, lemon and lime juice and shake until properly chilled, pour into a highball glass and top with coke. Optionally, garnish with lemon wheel.",
+      "Image": "CocktailPhotos\\BlueIslandIcedTea.jpg",
+      "ImageCredits": "This horrible photo was taken by Ronin on a crappy smart phone, more professional photos to come"
+    },
+    {
+      "Name": "Blue Island Iced Tea 4",
+      "Description": "The Blue Island Iced Tea is a simple modification of the Long Island Iced tea, simply replacing the triple sec with blue curacao. Due to the similar taste profiles, the taste of the drink isn't affected, however the result is a drink which naturally creates beautifully contrasted layers, while still remaining a tasty classic",
+      "Ingredients": [
+        {
+          "Name": "Vodka",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Gin",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "White Rum",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Tequila",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Blue Curacao",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Lemon Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Lime Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Coke",
+          "Amount": "To Top"
+        }
+      ],
+      "Method": "In a shaker, combine all the spirits, lemon and lime juice and shake until properly chilled, pour into a highball glass and top with coke. Optionally, garnish with lemon wheel.",
+      "Image": "CocktailPhotos\\BlueIslandIcedTea.jpg",
+      "ImageCredits": "This horrible photo was taken by Ronin on a crappy smart phone, more professional photos to come"
+    },
+    {
+      "Name": "Blue Island Iced Tea 5",
+      "Description": "The Blue Island Iced Tea is a simple modification of the Long Island Iced tea, simply replacing the triple sec with blue curacao. Due to the similar taste profiles, the taste of the drink isn't affected, however the result is a drink which naturally creates beautifully contrasted layers, while still remaining a tasty classic",
+      "Ingredients": [
+        {
+          "Name": "Vodka",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Gin",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "White Rum",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Tequila",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Blue Curacao",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Lemon Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Lime Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Coke",
+          "Amount": "To Top"
+        }
+      ],
+      "Method": "In a shaker, combine all the spirits, lemon and lime juice and shake until properly chilled, pour into a highball glass and top with coke. Optionally, garnish with lemon wheel.",
+      "Image": "CocktailPhotos\\BlueIslandIcedTea.jpg",
+      "ImageCredits": "This horrible photo was taken by Ronin on a crappy smart phone, more professional photos to come"
+    },
+    {
+      "Name": "Blue Island Iced Tea 6",
+      "Description": "The Blue Island Iced Tea is a simple modification of the Long Island Iced tea, simply replacing the triple sec with blue curacao. Due to the similar taste profiles, the taste of the drink isn't affected, however the result is a drink which naturally creates beautifully contrasted layers, while still remaining a tasty classic",
+      "Ingredients": [
+        {
+          "Name": "Vodka",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Gin",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "White Rum",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Tequila",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Blue Curacao",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Lemon Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Lime Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Coke",
+          "Amount": "To Top"
+        }
+      ],
+      "Method": "In a shaker, combine all the spirits, lemon and lime juice and shake until properly chilled, pour into a highball glass and top with coke. Optionally, garnish with lemon wheel.",
+      "Image": "CocktailPhotos\\BlueIslandIcedTea.jpg",
+      "ImageCredits": "This horrible photo was taken by Ronin on a crappy smart phone, more professional photos to come"
+    },
+    {
+      "Name": "Blue Island Iced Tea 7",
+      "Description": "The Blue Island Iced Tea is a simple modification of the Long Island Iced tea, simply replacing the triple sec with blue curacao. Due to the similar taste profiles, the taste of the drink isn't affected, however the result is a drink which naturally creates beautifully contrasted layers, while still remaining a tasty classic",
+      "Ingredients": [
+        {
+          "Name": "Vodka",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Gin",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "White Rum",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Tequila",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Blue Curacao",
+          "Amount": "15ml"
+        },
+        {
+          "Name": "Lemon Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Lime Juice",
+          "Amount": "30ml"
+        },
+        {
+          "Name": "Coke",
+          "Amount": "To Top"
+        }
+      ],
+      "Method": "In a shaker, combine all the spirits, lemon and lime juice and shake until properly chilled, pour into a highball glass and top with coke. Optionally, garnish with lemon wheel.",
+      "Image": "CocktailPhotos\\BlueIslandIcedTea.jpg",
+      "ImageCredits": "This horrible photo was taken by Ronin on a crappy smart phone, more professional photos to come"
     }
   ]
 }
